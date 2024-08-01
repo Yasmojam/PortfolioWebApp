fa0cada1c03ddfe0da8fd387941d6d0e0c54e892
Yasmojam <yazzij97@gmail.com>
2024-08-01 22:08:23 +0100

    margin portfolio page

--- src/components/PortfolioPage.tsx
+++ src/components/PortfolioPage.tsx
@@ -23,6 +23,7 @@ const PortfolioPage = () => {
                     display: 'flex',
                     flex: 1,
                     flexDirection: 'column',
+                    margin: '0 8px',
                 }}
             >
                 <CollectionsGrid
